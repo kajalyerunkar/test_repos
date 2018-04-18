f73dc7fdaac7cabf9bb11aed751cb75177c92f12
kajalyerunkar <37833204+kajalyerunkar@users.noreply.github.com>
2018-04-18 12:32:47 +0530

    Add files via upload

---
+++ shoppers_stop.js
@@ -0,0 +1,8 @@
+// $(document).ready(function(){
+// 		$("img").hide();
+// 		$("img:first").show();
+// 		setInterval(function(){
+// 			// alert(1);
+// 			$("img:first").fadeOut(2000).next().fadeIn(2000).end().appendTo(".banner");
+// 		},3000)
+// 	})
