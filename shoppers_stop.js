8bc3ea0692e71a2d49a1952e6ea47b3d7fc4aa0f
kajalyerunkar <37833204+kajalyerunkar@users.noreply.github.com>
2018-04-23 21:56:38 +0530

    Add files via upload

--- shoppers_stop.js
+++ shoppers_stop.js
@@ -1,8 +1,40 @@
-// $(document).ready(function(){
+$(document).ready(function(){
 // 		$("img").hide();
 // 		$("img:first").show();
 // 		setInterval(function(){
 // 			// alert(1);
 // 			$("img:first").fadeOut(2000).next().fadeIn(2000).end().appendTo(".banner");
 // 		},3000)
-// 	})
+
+	$(".blackbox").hide();
+
+	$(".pro_img").mouseenter(function(){
+		$(this).children(".blackbox").fadeIn(1000);
+
+	})
+
+	$(".pro_img").mouseleave(function(){
+		$(this).children(".blackbox").fadeOut(1000);
+	})
+	
+
+
+	//menu list tab
+	$(".menu_list_contents").hide();
+	// $(".menu_list_contents:first").show();
+
+	$(".menu_list li").mouseenter(function(){
+		position = $(this).index();
+		// alert(position);
+		$(".menu_list_contents").hide();
+		$(".menu_list_contents").eq(position).slideDown(500);
+	})
+	$(".menu_list li").mouseleave(function(){
+		position = $(this).index();
+		// alert(position);
+		$(".menu_list_contents").hide();
+		$(".menu_list_contents").eq(position).slideUp(500);
+	})
+	// tab examples
+ 	
+ })
